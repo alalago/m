0a97daed619362a1542d00581a0a91bf34ad4fe3
alalago <51186568+alalago@users.noreply.github.com>
2019-07-30 17:30:00 +0800

    Add files via upload

---
+++ images/main.js
@@ -0,0 +1,21 @@
+// JavaScript Document
+
+	
+	
+$(window).mouseover(function(evt){
+	
+	var pagex=evt.pageX;
+	var pagey=evt.pageY;
+	
+	
+	
+	
+	
+});
+	
+
+
+//mobile :active
+document.addEventListener("touchstart", function(){}, true);	
+	
+
