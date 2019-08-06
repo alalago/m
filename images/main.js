130c9327723e9158f4fddf4db0ec0fc4b55379b3
alalago <51186568+alalago@users.noreply.github.com>
2019-08-07 00:13:13 +0800

    Add files via upload

--- images/main.js
+++ images/main.js
@@ -6,47 +6,50 @@ document.addEventListener("touchstart", function() {},false);
 
 
 //
-w= document.body.clientWidth;
-h= document.body.clientHeight;
-var hw= w/2
-var hh= h/2
+w= window.innerWidth;
+h= window.innerHeight;
+var hw= w/2;
+var hh= h/2;
 
 
   $(window).resize(function() {
-    w= window.innerWidth;
-    h= window.innerHeight;
-	var hw= w/2
-    var hh= h/2
-
+    w=window.innerWidth;
+    h=window.innerHeight;
+	var hw= w/2;
+    var hh= h/2;
   });
 
 $(document).mouseover(function(evt){
-var pagex=evt.pageX;
-var pagey=evt.pageY;
+  var pagex=evt.pageX,
+      pagey=evt.pageY;
 
- if($(window).width() >=1440 )
-    {	
-		
-		
-
-	$(".building").css("transform","translate3d("+((pagex-hw)/100)+"%,"+((pagey-hh)/100)+"%,0)")	
-	$(".green01").css("transform","translate3d("+((pagex-hw)/100)+"%,"+((pagey-hh)/100)+"%,0)")	
-	$(".green02").css("transform","translate3d("+((pagex-hw)/100)+"%,"+((pagey-hh)/100)+"%,0)")	
-	$(".rainbow").css("transform","translate3d("+((pagex-hw)/100)+"%,"+((pagey-hh)/100)+"%,0)")	
-	$(".mypic").css("transform","translate3d("+((pagex-hw)/100)+"%,"+((pagey-hh)/100)+"%,0)")	
-	$(".train").css("transform","translate3d("+((pagex-hw)/100)+"%,"+((pagey-hh)/100)+"%,0)")	
-	$(".yellowline").css("transform","translate3d("+((pagex-hw)/100)+"%,"+((pagey-hh)/100)+"%,0)")	
-
-
-		
-
-
+ if($(window).width() >=1024 ){	
+	$(".building").css("transform","translate3d("+((pagex-hw)/100)+"%,"+((pagey-hh)/100)+"%,0)");	
+	$(".green01").css("transform","translate3d("+((pagex-hw)/100)+"%,"+((pagey-hh)/100)+"%,0)");	
+	$(".green02").css("transform","translate3d("+((pagex-hw)/100)+"%,"+((pagey-hh)/100)+"%,0)");	
+	$(".rainbow").css("transform","translate3d("+((pagex-hw)/100)+"%,"+((pagey-hh)/100)+"%,0)");	
+	$(".mypic").css("transform","translate3d("+((pagex-hw)/100)+"%,"+((pagey-hh)/100)+"%,0)");	
+	$(".train").css("transform","translate3d("+((pagex-hw)/100)+"%,"+((pagey-hh)/100)+"%,0)");	
+	$(".yellowline").css("transform","translate3d("+((pagex-hw)/100)+"%,"+((pagey-hh)/100)+"%,0)");	
 	}
 	else{}
 	
 });
 	
 
+  window.addEventListener('deviceorientation', function(event) {
+    var alpha = event.alpha,
+        beta = event.beta,
+        gamma = event.gamma;
+	$(".building").css("transform","translate3d("+gamma+"%,"+beta+"%,0)");	
+	$(".green01").css("transform","translate3d("+gamma+"%,"+beta+"%,0)");	
+	$(".green02").css("transform","translate3d("+gamma+"%,"+beta+"%,0)");	
+	$(".rainbow").css("transform","translate3d("+gamma+"%,"+beta+"%,0)");	
+	$(".mypic").css("transform","translate3d("+gamma+"%,"+beta+"%,0)");	
+	$(".train").css("transform","translate3d("+gamma+"%,"+beta+"%,0)");	
+	$(".yellowline").css("transform","translate3d("+gamma+"%,"+beta+"%,0)");	
+
+  }, false);
 
 
 
