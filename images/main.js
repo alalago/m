8f3226510a524d998f8fd76a20fce86877a7ccd3
alalago <51186568+alalago@users.noreply.github.com>
2019-08-06 23:08:49 +0800

    Add files via upload

--- images/main.js
+++ images/main.js
@@ -1,20 +1,53 @@
 // JavaScript Document
 
 	
-	
-$(window).mouseover(function(evt){
-	
-	var pagex=evt.pageX;
-	var pagey=evt.pageY;
-	
-	
-	
-	
+//mobile :active	
+document.addEventListener("touchstart", function() {},false);
+
+
+//
+w= document.body.clientWidth;
+h= document.body.clientHeight;
+var hw= w/2
+var hh= h/2
+
+
+  $(window).resize(function() {
+    w= window.innerWidth;
+    h= window.innerHeight;
+	var hw= w/2
+    var hh= h/2
+
+  });
+
+$(document).mouseover(function(evt){
+var pagex=evt.pageX;
+var pagey=evt.pageY;
+
+ if($(window).width() >=1440 )
+    {	
+		
+		
+
+	$(".building").css("transform","translate3d("+((pagex-hw)/100)+"%,"+((pagey-hh)/100)+"%,0)")	
+	$(".green01").css("transform","translate3d("+((pagex-hw)/100)+"%,"+((pagey-hh)/100)+"%,0)")	
+	$(".green02").css("transform","translate3d("+((pagex-hw)/100)+"%,"+((pagey-hh)/100)+"%,0)")	
+	$(".rainbow").css("transform","translate3d("+((pagex-hw)/100)+"%,"+((pagey-hh)/100)+"%,0)")	
+	$(".mypic").css("transform","translate3d("+((pagex-hw)/100)+"%,"+((pagey-hh)/100)+"%,0)")	
+	$(".train").css("transform","translate3d("+((pagex-hw)/100)+"%,"+((pagey-hh)/100)+"%,0)")	
+	$(".yellowline").css("transform","translate3d("+((pagex-hw)/100)+"%,"+((pagey-hh)/100)+"%,0)")	
+
+
+		
+
+
+	}
+	else{}
 	
 });
 	
 
 
-//mobile :active	
-document.addEventListener("touchstart", function() {},false);
+
+
 
