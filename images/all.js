9042e23746e6afd05e225fff9ed36ceae9985d94
alalago <51186568+alalago@users.noreply.github.com>
2019-08-19 22:04:25 +0800

    Add files via upload

---
+++ images/all.js
@@ -0,0 +1,44 @@
+// JavaScript Document
+
+
+
+
+//mobile :active	
+document.addEventListener("touchstart", function() {},false);
+
+
+//漢堡選單
+$('.nav span').click( function(){
+	$('.nav ul').slideToggle(300);
+});
+
+
+//視窗縮放重整
+$(window).resize(function() {
+		location.reload();
+});
+
+
+//
+
+
+
+var w = window.innerWidth;
+var th = $(".text").get(0).offsetHeight;
+var ph =$(".about .pic img").get(0).offsetHeight;
+
+$(window).ready(function(){
+	if( w <= 595){
+		$(".dot2").css({"top":+(th+ph*0.8)+"px","width":+(ph/1.2)+"px","height":+(ph/1.2)+"px"});
+		$(".dot3").css({"top":+(th+ph*0.4)+"px","width":+(ph/4.5)+"px","height":+(ph/4.5)+"px"});
+		$(".dot4").css({"top":+(th+ph*1.45)+"px","width":+(ph/6.5)+"px","height":+(ph/6.5)+"px"});
+		
+		
+		
+	}
+	
+	else{}
+	
+});
+	
+
